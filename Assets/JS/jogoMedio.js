6cdaab4d4424a115cde49ccc758a355655ab4f20
Gustavokvs <gustavokruger514@gmail.com>
2024-12-17 20:23:45 -0300

    Arrumado caminhos_16

--- Assets/JS/jogoMedio.js
+++ Assets/JS/jogoMedio.js
@@ -93,7 +93,7 @@ const createCard = (personagens) => {
     const front = createElement('div', 'face front');
     const back = createElement('div', 'face back');
 
-    front.style.backgroundImage = `url(' ../Assets/IMG/${personagens}.png')`;
+    front.style.backgroundImage = `url('./Assets/IMG/${personagens}.png')`;
 
     card.appendChild(front);
     card.appendChild(back);
