502032774b53c8ab4b1a33777e6b91763a3e5de9
Gustavokvs <gustavokruger514@gmail.com>
2024-12-17 20:21:24 -0300

    Arrumado caminhos_15

--- Assets/JS/jogoDificil.js
+++ Assets/JS/jogoDificil.js
@@ -17,7 +17,7 @@ const personagens = [
 
 let primeiraCarta = '';
 let segundaCarta = '';
-let loop; 
+let loop;
 
 
 const createElement = (tag, className) => {
@@ -34,19 +34,19 @@ const checkEndGame = () => {
     if (timer.innerHTML == 0) {
         setTimeout(() => {
             alert('Sinto muito, mas vc tem uma memória de peixe.');
-            stopTimer(); 
+            stopTimer();
         }, 1000)
         setTimeout(() => {
-            window.location.href = 'inicio.html'; 
+            window.location.href = 'inicio.html';
         }, 4000)
     } else if (disableCards.length == 20) {
         setTimeout(() => {
 
             alert(`Parabéns, ${spanPlayer.innerHTML} você conseguiria se lembrar do aniversário do seu cachorro kkkkk`);
-            stopTimer(); 
+            stopTimer();
         }, 1000)
         setTimeout(() => {
-            window.location.href = 'inicio.html'; 
+            window.location.href = 'inicio.html';
         }, 4000)
 
     }
@@ -62,7 +62,7 @@ const checarCarta = () => {
         segundaCarta.firstChild.classList.add('disable-card');
         primeiraCarta = '';
         segundaCarta = '';
-        checkEndGame(); 
+        checkEndGame();
     } else {
         setTimeout(() => {
             primeiraCarta.classList.remove('reveal-card');
@@ -85,7 +85,7 @@ const revealCard = ({ target }) => {
     } else if (segundaCarta == '') {
         target.parentNode.classList.add('reveal-card');
         segundaCarta = target.parentNode;
-        checarCarta(); 
+        checarCarta();
     }
 }
 
@@ -95,7 +95,7 @@ const createCard = (personagens) => {
     const front = createElement('div', 'face front');
     const back = createElement('div', 'face back');
 
-    front.style.backgroundImage = `url('../Assets/IMG/${personagens}.png')`;
+    front.style.backgroundImage = `url('./Assets/IMG/${personagens}.png')`;
 
     card.appendChild(front);
     card.appendChild(back);
@@ -121,8 +121,8 @@ const startTimer = () => {
     loop = setInterval(() => {
         const currentTime = +timer.innerHTML;
         if (currentTime <= 0) {
-            clearInterval(loop); 
-            checkEndGame(); 
+            clearInterval(loop);
+            checkEndGame();
         } else {
             timer.innerHTML = currentTime - 1;
         }
@@ -131,16 +131,16 @@ const startTimer = () => {
 
 
 const stopTimer = () => {
-    clearInterval(loop); 
+    clearInterval(loop);
 }
 
 
 const startGame = () => {
-    
+
     const playerName = localStorage.getItem('player');
     spanPlayer.innerHTML = playerName;
 
-    
+
     startTimer();
     carregarJogo();
 }
